52ce4a80f80a00fd1c34e31e01ce627f34a1bb50
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-30 11:28:24 -0300

    Reestruturando a base do exercício...

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,17 @@
+export default function Cabecalho(){
+
+    return(
+        <>
+            <header>
+                <h1>Vite+React</h1>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Produtos</li>
+                        <li>Editar</li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
