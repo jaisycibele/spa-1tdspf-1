78bfe541fec2cca64802115ab1dd107d15e651f9
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-10-04 11:28:28 -0300

    iniciando o post do produto

--- src/components/ModalInserir/ModalInserir.jsx
+++ src/components/ModalInserir/ModalInserir.jsx
@@ -1,61 +1,94 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./ModalInserir.module.css";
 import "./ModalInserir.scss";
 
 export default function ModalInserir(props) {
+  document.title = "CADASTRO";
 
-    document.title = "CADASTRO";
+  let novoId;
 
-    let novoId;
-useEffect(()=>{
-    fetch("http://localhost:5000/produtos",{
-        method:"GET",
-        headers:{
-            "Content-Type":"application/json"
-        }
+  useEffect(() => {
+    fetch("http://localhost:5000/produtos", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .then((response)=> response.json())
-    .then((data)=>{
-        console.log(data)
-        novoId = (data[data.length-1].id+1)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        novoId = data[data.length - 1].id + 1;
         return novoId;
-        })
-    .catch(error=> console.log(error));
-}, [])
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
- if (props.open) {
+  const [produto, setProduto] = useState({
+    id:novoId,
+    nome:"",
+    desc:"",
+    preco:"",
+    img:""
+  });
+
+  
+
+
+  
+  if (props.open) {
     return (
       <div className={styles.container}>
         <h1>CADASTRO DE PRODUTOS</h1>
 
         <div className="formGroup">
           <form>
-              <fieldset>
-        <span className="btnClose" onClick={()=> props.setOpen(false)}>X</span>
-                <legend>Novo Produto</legend>
-                <div>
-                  <label htmlFor="idNome">Nome</label>
-                  <input type="text" name="nome" id="idNome" placeholder="Digite o nome do produto."/>
-                </div>
-                <div>
-                  <label htmlFor="idDesc">Descrição</label>
-                  <input type="text" name="desc" id="idDesc" placeholder="Digite a descrição do produto."/>
-                </div>
-                <div>
-                  <label htmlFor="idPreco">Preço</label>
-                  <input type="number" name="preco" id="idPreco" placeholder="Digite o valor do produto."/>
-                </div>                
-                <div>
-                  <label htmlFor="idImg">Imagem</label>
-                  <input type="url" name="img" id="idImg" placeholder="Digite a url da imagem do produto."/>
-                </div>                
-                <div>
-                  <button>CADASTRAR</button>
-                </div>                
-              </fieldset>
+            <fieldset>
+              <span className="btnClose" onClick={() => props.setOpen(false)}>
+                X
+              </span>
+              <legend>Novo Produto</legend>
+              <div>
+                <label htmlFor="idNome">Nome</label>
+                <input
+                  type="text"
+                  name="nome"
+                  id="idNome"
+                  placeholder="Digite o nome do produto."
+                />
+              </div>
+              <div>
+                <label htmlFor="idDesc">Descrição</label>
+                <input
+                  type="text"
+                  name="desc"
+                  id="idDesc"
+                  placeholder="Digite a descrição do produto."
+                />
+              </div>
+              <div>
+                <label htmlFor="idPreco">Preço</label>
+                <input
+                  type="number"
+                  name="preco"
+                  id="idPreco"
+                  placeholder="Digite o valor do produto."
+                />
+              </div>
+              <div>
+                <label htmlFor="idImg">Imagem</label>
+                <input
+                  type="url"
+                  name="img"
+                  id="idImg"
+                  placeholder="Digite a url da imagem do produto."
+                />
+              </div>
+              <div>
+                <button>CADASTRAR</button>
+              </div>
+            </fieldset>
           </form>
         </div>
-
       </div>
     );
   }
