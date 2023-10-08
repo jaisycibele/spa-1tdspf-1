e8c70d666fb55064a3e214b8e21bf1fd18130d79
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-10-08 16:51:18 -0300

    Adicionando a edição e exclusão

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,29 +1,29 @@
-import { useEffect, useState } from "react";
-import { useParams, useNavigate } from 'react-router-dom'
-import { ListaProdutos } from '../components/ListaProdutos';
-import styles from './Produtos.module.css'
- 
-export default function EditarProduto() {
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+export default function EditarProdutos() {
+  document.title = "EDITAR PRODUTO";
+
+  const navigate = useNavigate();
   const { id } = useParams();
   const [produto, setProduto] = useState({
-    id: id,
-    nome: "",
-    desc: "",
-    preco: "",
-    img: "",
+    id: '',
+    nome: '',
+    desc: '',
+    img: '',
+    preco: '',
   });
 
   useEffect(() => {
-    // Buscar o produto com o ID especificado da API
     fetch(`http://localhost:5000/produtos/${id}`, {
-      method: "GET",
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     })
       .then((response) => response.json())
       .then((data) => {
-        setProduto(data); // Preenche o estado com os dados do produto
+        setProduto(data);
       })
       .catch((error) => console.log(error));
   }, [id]);
@@ -31,86 +31,50 @@ export default function EditarProduto() {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setProduto({ ...produto, [name]: value });
-  };
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Enviar a solicitação PUT para atualizar o produto
     fetch(`http://localhost:5000/produtos/${id}`, {
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(produto),
     })
       .then((response) => response.json())
-      .then((response) => {
-        console.log(response); // Exibe a resposta da API
-        // Redireciona para a página de produtos após a edição
-        window.location = "/produtos";
+      .then(() => {
+        // Redirecionar após a edição ser concluída
+        navigate('/produtos');
       })
       .catch((error) => console.log(error));
-  };
+  }
 
   return (
-    <div className={styles.container}>
-      <h1>EDITAR PRODUTO</h1>
-
-      <div className={styles.formGroup}>
+    <>
+      <div>
         <form onSubmit={handleSubmit}>
           <fieldset>
             <legend>Editar Produto</legend>
             <div>
               <label htmlFor="idNome">Nome</label>
-              <input
-                type="text"
-                name="nome"
-                id="idNome"
-                placeholder="Digite o nome do produto."
-                value={produto.nome}
-                onChange={handleChange}
-              />
+              <input type="text" name="nome" id="idNome" value={produto.nome} onChange={handleChange} />
             </div>
             <div>
               <label htmlFor="idDesc">Descrição</label>
-              <input
-                type="text"
-                name="desc"
-                id="idDesc"
-                placeholder="Digite a descrição do produto."
-                value={produto.desc}
-                onChange={handleChange}
-              />
+              <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handleChange} />
             </div>
             <div>
               <label htmlFor="idPreco">Preço</label>
-              <input
-                type="number"
-                name="preco"
-                id="idPreco"
-                placeholder="Digite o valor do produto."
-                value={produto.preco}
-                onChange={handleChange}
-              />
+              <input type="text" name="preco" id="idPreco" value={produto.preco} onChange={handleChange} />
             </div>
             <div>
-              <label htmlFor="idImg">Imagem</label>
-              <input
-                type="url"
-                name="img"
-                id="idImg"
-                placeholder="Digite a url da imagem do produto."
-                value={produto.img}
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <button>EDITAR</button>
+              <button type="submit">EDITAR</button>
             </div>
           </fieldset>
         </form>
       </div>
-    </div>
+    </>
   );
 }
--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -1,52 +1,85 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { ListaProdutos } from "../components/ListaProdutos";
-import style from "./ExcluirProdutos.module.css";
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import style from './ExcluirProdutos.module.css'
+import './ExcluirProduto.scss'
 
 export default function ExcluirProdutos() {
-
-  document.title = "EXCLUIR PRODUTO";
+  document.title = 'EXCLUIR PRODUTO';
 
   const navigate = useNavigate();
   const { id } = useParams();
-  const produto = ListaProdutos.filter((produto) => produto.id == id)[0];
+  const [produto, setProduto] = useState({
+    id: '',
+    nome: '',
+    desc: '',
+    img: '',
+    preco: '',
+  });
 
-  const handleDelete = ()=>{
-    
-    let indice;
-    //Ou utilizando o método findIndex
-    indice = ListaProdutos.findIndex(item => item.id == produto.id);
+  useEffect(() => {
+    // Carregue os detalhes do produto pelo ID
+    fetch(`http://localhost:5000/produtos/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Produto não encontrado');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProduto(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        navigate('/produtos'); // Redirecionar em caso de erro
+      });
+  }, [id, navigate]);
 
-    //Alterando o produto na lista com o método splice()
-    ListaProdutos.splice(indice,1);
-
-    alert("Produto excluído com SUCESSO!");
-
-    //Redirecionando o usuáio para a página de produtos!
-    navigate('/produtos');
-  }
+  const handleDelete = () => {
+    // Enviar solicitação DELETE para excluir o produto
+    fetch(`http://localhost:5000/produtos/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Ocorreu um erro ao excluir o produto');
+        }
+        // Produto excluído com sucesso
+        alert('Produto excluído com sucesso!');
+        navigate('/produtos');
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Ocorreu um erro ao excluir o produto');
+      });
+  };
 
   return (
     <>
-        <div>
-          <h1 className={style.tit}>Excluir Produtos</h1>
-          
-          <div className={style.card}>
-            <h2>Produto Selecionado</h2>
-            <h3 className={style.alert}>Deseja realmente excluir esse produto?</h3>
-            <figure>
-              <img src={produto.img} alt={produto.desc} />
-              <figcaption>{produto.nome} - <span>R$ {produto.preco}</span></figcaption>
-            </figure>
-            
-            <div className={style.btn}>
-              <button onClick={handleDelete}>EXCLUIR</button>
-              <button onClick={()=> navigate("/produtos")}>CANCELAR</button>
-            </div>
-
+      <div>
+        <h1 className={style.tit}>Excluir Produto</h1>
+       
+        <div className={style.card}>
+          <h2>Produto Selecionado</h2>
+          <h3 className={style.alert}>Deseja realmente excluir esse produto?</h3>
+          <p>ID: {produto.id}</p>
+          <figure>
+            <img src={produto.img} alt={produto.desc} />
+            <figcaption>{produto.nome} - <span>R$ {produto.preco}</span></figcaption>
+          </figure>
+          <div className={style.btn}>
+            <button onClick={handleDelete}>EXCLUIR</button>
+            <button onClick={() => navigate('/produtos')}>CANCELAR</button>
           </div>
-
         </div>
+  </div>
     </>
-    
-  )
+  );
 }
