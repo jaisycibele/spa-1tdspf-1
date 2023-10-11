0839d931b3bad1563a1256e518491c0a094c2e3f
jaisycibele <jaisycibele@gmail.com>
2023-10-10 23:56:09 -0300

    Estilizando o rodapé.

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,19 +1,20 @@
+import "./Rodape.scss"
+
 export default function Rodape() {
   return (
     <>
       <footer>
-        <div>
-          <ul>
-            <li>
-              <a href="#">GitHub</a>
-            </li>
-            <li>
-              <a href="#">X</a>
-            </li>
-            <li>
-              <a href="#">Reddit</a>
-            </li>
-          </ul>
+        <div className="rodape">
+          <a href="#">
+              <img src="/github.png" alt="git"  className="imagem1" />
+              </a>
+              <a href="#">
+              <img src="/x.png" alt="x" className="imagem2" />
+              </a>
+              <a href="#">
+                <img src="/reddit.png" alt="reddit" className="imagem3" />
+                </a>
+            
         </div>
       </footer>
     </>
